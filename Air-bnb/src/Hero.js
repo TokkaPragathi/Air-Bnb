e49302a52f92e2378c4bedbbe1240a6c24e07fd1
TokkaPragathi <121242054+TokkaPragathi@users.noreply.github.com>
2024-03-07 10:44:13 +0530

    Add files via upload

---
+++ Air-bnb/src/Hero.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Hero(){
+    return(
+        <section className="Hero">
+            <img src="photo-grid.png" className="Hero--logo"></img>
+            <h1 className="Hero--header">Online Experiences</h1>
+            <p className="Hero--text">Join unique interactive activities led by 
+            one-of-a-kind hosts—all without leaving home.</p>
+        </section>
+    )
+}
---
+++ Air-bnb/src/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src="Airbnb_Logo.png" className="nav--logo" alt=" "/>
+        </nav>
+    )
+}
